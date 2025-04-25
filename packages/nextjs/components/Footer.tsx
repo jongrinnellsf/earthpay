c4dc8caf7192b4ed3a0bc70a22d2c9ae73f6eb7e
Christopher Hranj <christopher.hranj@alchemy.com>
2025-04-25 15:05:37 -0400

    fix font color on token balance list. fix font color and simplify component on footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -5,37 +5,32 @@ import { HeartIcon } from "@heroicons/react/24/outline";
  * Site footer
  */
 export const Footer = () => {
-
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
-      <div className="w-full">
-        <ul className="menu menu-horizontal w-full">
-          <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <div className="flex items-center justify-center">
-              <p className="text-center">
-                Forked with <HeartIcon className="inline-block h-4 w-4 text-indigo-500" /> from{" "}
-                <a
-                  href="https://github.com/alchemyplatform/scaffold-alchemy"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-medium text-indigo-500 hover:text-indigo-600 transition-colors"
-                >
-                  Scaffold Alchemy
-                </a>{" "}
-                <a
-                  href="https://docs.alchemy.com/docs/scaffold-alchemy"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-xs text-gray-500 hover:text-gray-600 transition-colors"
-                >
-                  (docs)
-                </a>. This is a demo app - no real money is involved.
-              </p>
-            </div>
-          </div>
-        </ul>
+      <div className="flex justify-center w-full">
+        <p className="flex items-center gap-1">
+          <span className="text-gray-500 text-sm">
+            Forked with <HeartIcon className="inline-block h-4 w-4 text-indigo-500" /> from{" "}
+            <a
+              href="https://github.com/alchemyplatform/scaffold-alchemy"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium text-indigo-500 hover:text-indigo-600 transition-colors"
+            >
+              Scaffold Alchemy
+            </a>{" "}
+            <a
+              href="https://docs.alchemy.com/docs/scaffold-alchemy"
+              target="_blank"
+              rel="noreferrer"
+              className="text-xs text-gray-500 hover:text-gray-600 transition-colors"
+            >
+              (docs)
+            </a>
+            . This is a demo app - no real money is involved.
+          </span>
+        </p>
       </div>
     </div>
   );
 };
-
